050bdf47dbe719c1b1a2e093e0a653d7a957e320
sebalp1987 <sebalp_1987@hotmail.com>
2020-01-22 14:36:13 +0100

    Init Commit

--- etl/etl_job.py
+++ etl/etl_job.py
@@ -2,7 +2,8 @@ import pandas as pd
 from resources import STRING
 from datetime import datetime, timedelta
 from googleads import ad_manager, oauth2, errors
-
+from dateutil.relativedelta import relativedelta
+import tempfile
 
 class EtlAds:
     pd.set_option('display.max_columns', 500)
@@ -20,41 +21,36 @@ class EtlAds:
     def _extract(self):
         df = pd.read_csv(self.input_file, sep=';')
 
-        # Create Report
-        statement = (ad_manager.StatementBuilder(version='v201908')
-                     .Where('ORDER_ID = :id')
-                     .WithBindVariable('id', int(1))
+        ad_manager_client = ad_manager.AdManagerClient.LoadFromStorage(STRING.GOOGLEADS_YAML_FILE)
+        network_service = ad_manager_client.GetService('NetworkService')
+        current_network = network_service.getCurrentNetwork()
+        root_ad_unit_id = (
+            network_service.getCurrentNetwork()['effectiveRootAdUnitId'])
+        print(root_ad_unit_id)
+        # Create statement object to filter for an order.
+        statement = (ad_manager.StatementBuilder(version='v201905')
+                     .Where('AD_UNIT_ID = :AdUnitId')
+                     .WithBindVariable('AdUnitId', int(root_ad_unit_id))
                      .Limit(None)  # No limit or offset for reports
                      .Offset(None))
 
+        # Set the start and end dates of the report to run
         end_date = datetime.now().date()
-        start_date = end_date - timedelta(days=8)
-
+        start_date = end_date - timedelta(days=365)
+        print(end_date)
+        print(start_date)
         # Create report job.
         report_job = {
             'reportQuery': {
-                'dimensions': ['ORDER_ID', 'ORDER_NAME'],
-                'dimensionAttributes': ['ORDER_TRAFFICKER', 'ORDER_START_DATE_TIME',
-                                        'ORDER_END_DATE_TIME'],
-                'statement': statement.ToStatement(),
-                'columns': ['AD_SERVER_IMPRESSIONS', 'AD_SERVER_CLICKS',
-                            'AD_SERVER_CTR', 'AD_SERVER_CPM_AND_CPC_REVENUE',
-                            'AD_SERVER_WITHOUT_CPD_AVERAGE_ECPM'],
-                'dateRangeType': 'CUSTOM_DATE',
-                'startDate': start_date,
-                'endDate': end_date
+                'dimensions': ['DATE'],
+                'adUnitView': 'HIERARCHICAL',
+                'columns': ['AD_SERVER_IMPRESSIONS'],
+                'dateRangeType': 'LAST_3_MONTHS',
+                'statement': statement.ToStatement()
             }
         }
-        # Run Report
-        ad_manager_client = ad_manager.AdManagerClient.LoadFromStorage(STRING.GOOGLEADS_YAML_FILE)
-        networks = ad_manager_client.GetService('NetworkService').getAllNetworks()
-        print(networks)
-        for network in networks:
-            print('Network with network code "%s" and display name "%s" was found.'
-                  % (network['networkCode'], network['displayName']))
 
         report_downloader = ad_manager_client.GetDataDownloader(version='v201908')
-
         try:
             # Run the report and wait for it to finish.
             report_job_id = report_downloader.WaitForReport(report_job)
@@ -62,7 +58,20 @@ class EtlAds:
         except errors.AdManagerReportError as e:
             print('Failed to generate report. Error was: %s' % e)
 
-        print(report_job)
+        export_format = 'CSV_DUMP'
+
+        report_file = tempfile.NamedTemporaryFile(suffix='.csv.gz', delete=False)
+
+        # Download report data.
+        report_downloader.DownloadReportToFile(
+            report_job_id, export_format, report_file)
+
+        report_file.close()
+
+        # Display results.
+        print('Report job with id "%s" downloaded to:\n%s' % (
+            report_job_id, report_file.name))
+
         return df
 
     @staticmethod
